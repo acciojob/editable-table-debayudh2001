f5c96aaa378ee5f612980b3133c363c9177a8663
debayudh2001 <debayudh31@gmail.com>
2025-04-10 17:06:41 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,38 +3,26 @@ import React, { useMemo, useRef, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  //const [editedRows, setEditedRows] = useState([])
-   
   const editedRows = useRef([])
-
-  let rowIds = useMemo(() => {
-    let arr = []
-    for(let i=1; i<=6; i++){
-      arr.push(i)
-    }
-    return arr
-  }, [])
   
-  function handleChange(e, id){
-    if(e.target.name === "name"){
-      //setEditedRows([...editedRows, id])
-      editedRows.current.push(id)
-    }
-    if(e.target.name === "age"){
-      //setEditedRows([...editedRows, id])
-      editedRows.current.push(id)
-    }
-  }
-
-  function handleSaveChanges() {
-    //console.log(`Edited Rows: ${[...new Set(editedRows)].sort((x,y) => x-y)}`)
-    //setEditedRows([])
+  function handleSaveChanges(e){
+    e.preventDefault()
     console.log([...new Set(editedRows.current)].sort((x,y) => x-y))
     editedRows.current = []
   }
 
+  function handleChange(e, id){
+    if(e.target.name === "name"){
+      editedRows.current.push(id)
+    }
+    if(e.target.name === "age"){
+      editedRows.current.push(id)
+    }
+  }
+
   return (
     <div>
+      <form>
       <table>
         <thead>
           <tr>
@@ -44,19 +32,41 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-          {rowIds.map((id, ind) => {
-            return (
-              <tr key={ind}>
-                <td>{id}</td>
-                <td><input type="text" name="name" onChange={(e) => handleChange(e, id)} /></td>
-                <td><input type="number" name="age" onChange={(e) => handleChange(e, id)} /></td>
-              </tr>
-            )
-          })}
+          <tr>
+            <td>1</td>
+            <td><input type="text" name="name" defaultValue="Ram" onChange={(e) => handleChange(e, 1)}  /></td>
+            <td><input type="number" name="age" defaultValue="25" onChange={(e) => handleChange(e, 1)}  /></td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td><input type="text" name="name" defaultValue="Shyam" onChange={(e) => handleChange(e, 2)} /></td>
+            <td><input type="number" name="age" defaultValue="30" onChange={(e) => handleChange(e, 2)} /></td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td><input type="text" name="name" defaultValue="Ali" onChange={(e) => handleChange(e, 3)}  /></td>
+            <td><input type="number" name="age" defaultValue="35" onChange={(e) => handleChange(e, 3)}  /></td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td><input type="text" name="name" defaultValue="Shaw" onChange={(e) => handleChange(e, 4)} /></td>
+            <td><input type="number" name="age" defaultValue="20"  onChange={(e) => handleChange(e, 4)}/></td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td><input type="text" name="name" defaultValue="Tavneet" onChange={(e) => handleChange(e, 5)} /></td>
+            <td><input type="number" name="age" defaultValue="50" onChange={(e) => handleChange(e, 5)} /></td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td><input type="text" name="name" defaultValue="Laxshmi" onChange={(e) => handleChange(e, 6)} /></td>
+            <td><input type="number" name="age" defaultValue="40" onChange={(e) => handleChange(e, 6)}  /></td>
+          </tr>
         </tbody>
       </table>
       <br />
       <button onClick={handleSaveChanges}>Save Changes</button>
+      </form>
     </div>
   )
 }
