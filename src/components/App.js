46fb380a41028e1a7385fee698308e7097f810c9
debayudh2001 <debayudh31@gmail.com>
2025-04-10 15:53:31 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,62 @@
 
-import React from "react";
+import React, { useMemo, useRef, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  //const [editedRows, setEditedRows] = useState([])
+   
+  const editedRows = useRef([])
+
+  let rowIds = useMemo(() => {
+    let arr = []
+    for(let i=1; i<=6; i++){
+      arr.push(i)
+    }
+    return arr
+  }, [])
+  
+  function handleChange(e, id){
+    if(e.target.name === "name"){
+      //setEditedRows([...editedRows, id])
+      editedRows.current.push(id)
+    }
+    if(e.target.name === "age"){
+      //setEditedRows([...editedRows, id])
+      editedRows.current.push(id)
+    }
+  }
+
+  function handleSaveChanges() {
+    //console.log(`Edited Rows: ${[...new Set(editedRows)].sort((x,y) => x-y)}`)
+    //setEditedRows([])
+    console.log([...new Set(editedRows.current)].sort((x,y) => x-y))
+    editedRows.current = []
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rowIds.map((id, ind) => {
+            return (
+              <tr key={ind}>
+                <td>{id}</td>
+                <td><input type="text" name="name" onChange={(e) => handleChange(e, id)} /></td>
+                <td><input type="number" name="age" onChange={(e) => handleChange(e, id)} /></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <br />
+      <button onClick={handleSaveChanges}>Save Changes</button>
     </div>
   )
 }
